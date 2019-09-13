bc5d3171f644d01fb60bb7d638462a388b6fe7f5
Filip Mares <filipmares@gmail.com>
2019-09-13 11:08:32 -0700

    update to blobstore

--- export-google-play.js
+++ export-google-play.js
@@ -1,49 +1,60 @@
 const _logStyle = 'background: #222; color: #bada55';
 
 function log(message) {
-	console.log('%c ' + message, _logStyle);
+    console.log('%c ' + message, _logStyle);
 }
 
-log('Please scroll through the playlist so that each album is visible once.\n' + 
-      'Then double-click the page to export a spreadsheet.');
+log('Please scroll through the playlist so that each album is visible once.\n' + 'Then double-click the page to export a spreadsheet.');
 
-var albums = ['Artist,Title,Album'];
+var headers = ['Artist,Title,Album'];
+var albums = [...headers];
 
 var fileName = 'export-google-play.csv';
 
 var playlistTitleNode = document.querySelector('.info > .title > .title-text');
 if (playlistTitleNode) {
-	fileName = playlistTitleNode.innerText;
+    fileName = playlistTitleNode.innerText;
 }
 
-var addVisibleAlbums = function(){
-	[].forEach.call(document.querySelectorAll('.song-row'), function(e){ 
-		var albumNodes = [e.querySelector('td[data-col=\'artist\']'),
-				e.querySelector('td[data-col=\'title\'] .column-content'),
-				e.querySelector('td[data-col=\'album\']')];
+var addVisibleAlbums = function() {
+    debugger
+    [].forEach.call(document.querySelectorAll('.song-row'), function(e) {
+        var albumNodes = [e.querySelector('td[data-col=\'artist\']'), e.querySelector('td[data-col=\'title\'] .column-content'), e.querySelector('td[data-col=\'album\']')];
 
-		var albumString = albumNodes.map(function(s){ 
-			return s.innerText.trim().replace(/,/g,''); 
-		}).join(',');
- 
-		if(albums.indexOf(albumString) === -1){
-			albums.push(albumString); console.log('Added: ' + albumString)
-		}
-	});
+        var albumString = albumNodes.map(function(s) {
+            return s.innerText.trim().replace(/,/g, '');
+        }).join(',');
+
+        if (albums.indexOf(albumString) === -1) {
+            albums.push(albumString);
+            console.log('Added: ' + albumString)
+        }
+    });
 }
 
-var createCsv = function(){
-    var csv = 'data:text/csv;charset=utf-8,';
-    albums.forEach(function(row){ csv += row + '\n'; }); 
-
-    var uri = encodeURI(csv);
-    var link = document.createElement('a');
-    link.setAttribute('href', uri);
-    link.setAttribute('download', fileName);
-    document.body.appendChild(link);
-    link.click(); 
-
+var createCsv = function() {
     log('Download beginning!');
+
+    var csv = '';
+    albums.forEach(function(row) {
+        csv += row + '\n';
+    });
+
+    var blob = new Blob([csv],{
+        type: 'text/csv;charset=utf-8,%EF%BB%BF;'
+    });
+
+    var link = document.createElement("a");
+    if (link.download !== undefined) {
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", fileName);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        albums=[...headers]
+    }
 }
 
 document.body.addEventListener('DOMNodeInserted', addVisibleAlbums, false);
